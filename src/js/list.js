8c635646e3799b890b78d648c8d40bc8730a4d21
dming <dming.lin1996@gmail.com>
2019-10-19 10:01:13 +0800

    listpages

--- src/js/list.js
+++ src/js/list.js
@@ -1,44 +1,97 @@
-// let pages = document.getElementById('pages');
-let ipage = 1; //获取第一页数据
-let num = 25; //每页显示5条
-
-// 渲染数据
-
 $(() => {
-    function init() {
-        console.log(1);
-
-        function creat(arr) {
-            console.log(arr);
-            let html = arr.data.map(item => {
-                return `<li data-guid="${item.uid}"><img src="${item.img}" alt=""><div><p>${item.name}</p><span>￥ ${item.price}.00</span></div></li>`;
-            }).join('');
-            $("#thingList").html(html);
-
-            // 渲染页码
-            // let total = Math.ceil(arr.total / arr.num);
-            // let spanstr = '';
-            // for (let i = 0; i < total; i++) {
-            //     spanstr += `< a href=" ">${i + 1}</ a>`;
-            // }
-            // $("pages").html(spanstr);
-            // pages.innerHTML = spanstr;
-            // pages.children[ipage - 1].className = 'active';
-        }
+    new Promise(function (resolve, reject) {
         $.ajax({
-            type: 'get',
-            url: '../server/paging.php',
-            data: {
-                page: ipage,
-                num: num
-            },
-            success: str => {
-                let arr = JSON.parse(str);
-                console.log(arr);
-                creat(arr);
+            type: "get",
+            url: "../server/getPageCount.php",
+            dataType: "json",
+            success: (data) => {
+                let res = "";
+                for (let i = 0; i < data.count; i++) {
+                    res += `<a href="javascript:;">${i + 1}</a>`
+                }
+                $(".pages").html(res);
+                $(".pages").children().eq(0).addClass("active");
+
+                resolve();
             }
         });
+    }).then(function () {
+        getDataWithPage(1, 0);
+    })
 
+    /* type ==0 默认排序  id */
+    /* type ==1 升序排列  价格 */
+    /* type ==2 降序排列  价格 */
+    function getDataWithPage(page, type) {
+        $.ajax({
+            type: "get",
+            url: "../server/getGoodsData.php",
+            data: `page=${page}&sortType=${type}`,
+            dataType: "json",
+            success: (data) => renderUI(data)
+        });
     }
-    init();
+
+    function renderUI(data) {
+        console.log(data);
+
+        let html = data.map((ele) => `<li data-guid="${ele.uid}"><img src="${ele.img}" alt=""><div><p>${ele.name}</p><span>￥ ${ele.price}.00</span></div></li>`).join('');
+        $("#thingList").html(html);
+    }
+
+    /* 先给页面添加点击事件，当点击的时候获取页码值，根据该值发送网络请求 */
+    $(".pages").on("click", "a", function () {
+        getDataWithPage($(this).text());
+        $(this).addClass("active").siblings().removeClass("active");
+    })
+
+    /* 处理排序 */
+    $(".typeBtn").click(function () {
+        getDataWithPage(1, $(this).index());
+    })
+
+
+
+
+
+
+    // // 渲染数据
+    // let ipage = 1; //获取第一页数据
+    // let num = 20; //每页显示20条
+
+    // function init() {
+    //     // console.log(1);
+
+    //     function creat(arr) {
+    //         // console.log(arr);
+    //         let html = arr.data.map(item => `<li data-guid="${item.uid}"><img src="${item.img}" alt=""><div><p>${item.name}</p><span>￥ ${item.price}.00</span></div></li>`).join('');
+    //         $("#thingList").html(html);
+
+    //         // 渲染页码
+    //         let total = Math.ceil(arr.total / arr.num);
+    //         let spanstr = '';
+    //         for (let i = 0; i < total; i++) {
+    //             spanstr += `<a href="">${i+1}</a>`;
+    //         }
+    //         $(".pages").html(spanstr);
+    //         $(".pages").children().eq(ipage - 1).addClass("active").siblings().removeClass("active");
+    //     }
+
+
+    //     $.ajax({
+    //         type: 'get',
+    //         url: '../server/paging.php',
+    //         data: {
+    //             page: ipage,
+    //             num: num
+    //         },
+    //         success: str => {
+    //             let arr = JSON.parse(str);
+    //             // console.log(arr);
+    //             creat(arr);
+    //         }
+    //     });
+
+    // }
+    // init();
 })
--- src/server/getGoodsData.php
+++ src/server/getGoodsData.php
@@ -1,6 +1,6 @@
 <?php
 # 01-先连接数据库
-$db = mysqli_connect("127.0.0.1","root","root","suningshengxian");
+$db = mysqli_connect("127.0.0.1","root","","lining");
 
 # 获取参数
 $page = ($_REQUEST["page"] -1 ) * 20;
@@ -9,11 +9,11 @@ $type = $_REQUEST["sortType"];
 # 02-查询获取数据库所有的数据
 if($type == 0)
 {
-  $sql = "SELECT * FROM goods LIMIT $page, 20";
+  $sql = "SELECT * FROM list LIMIT $page, 20";
 }elseif($type == 1){
-  $sql = "SELECT * FROM goods ORDER BY price DESC LIMIT $page, 20";
+  $sql = "SELECT * FROM list ORDER BY price DESC LIMIT $page, 20";
 }else{
-  $sql = "SELECT * FROM goods ORDER BY price ASC LIMIT $page, 20";
+  $sql = "SELECT * FROM list ORDER BY price ASC LIMIT $page, 20";
 }
 
 $result = mysqli_query($db,$sql);
